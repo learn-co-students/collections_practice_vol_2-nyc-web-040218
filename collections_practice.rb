42245e73ced15c472b2664be94894f73d3860cf1
LaneMiller Miller <chris.lane.miller@gmail.com>
2018-03-20 20:39:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
-# your code goes here
+def begins_with_r(array)
+  i = 1
+  array.map do |word| 
+    if i < array.length && word.start_with?("r")
+      i += 1
+    elsif i = array.length && word.start_with?('r')
+      return true
+    else
+      return false
+    end
+  end
+end
+
+def contain_a(array)
+  array.find_all do |word|
+    word.include?('a')
+  end
+end
+
+def first_wa
+  array.find
+end
