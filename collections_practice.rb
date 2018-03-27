f297353b9eaf4848e608943e98c0bafd066e9e15
Lane Miller <chris.lane.miller@gmail.com>
2018-03-27 18:13:21 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,7 @@
+require 'pry'
 def begins_with_r(array)
   i = 1
+  
   array.map do |word| 
     if i < array.length && word.start_with?("r")
       i += 1
@@ -17,6 +19,59 @@ def contain_a(array)
   end
 end
 
-def first_wa
-  array.find
+def first_wa(array)
+  array.find do |word|
+    if word.to_s.start_with?('wa')
+      return word
+    end
+  end
 end
+
+
+#  ["blake", 1, :hello]
+
+def remove_non_strings(array)
+  output = []
+  array.each do |item|
+    if item.class == String
+      output << item
+    end
+  end
+  return output
+end
+
+def count_elements(array)
+  output = []
+  
+  counter = Hash.new(0)
+  array.each { |i| counter[i] += 1 }
+    counter.map do |key, value| 
+      output << {:name=>key.values.join(), :count => value}
+    end
+  return output
+end
+
+def merge_data(keys, data)
+  # If keys[index][:first_name] == data[index].keys[index], do keys[index][:first_name] = data[0]["first_name"]
+  # keys = [{:first_name=>"blake"}, {:first_name=>"ashley"}]
+  # data = [ {"blake"=>{:awesomeness=>10, :height=>"74", :last_name=>"johnson"},
+  #         "ashley"=>{:awesomeness=>9, :height=>60, :last_name=>"dubs"} } ]
+  
+  output = keys
+  i = 0
+  
+  while i < keys.length
+    data.each do |items|
+      items.each do |name, attributes|
+        if keys[i].values.join() == name
+          attributes.each do |key, value|
+            output[i][key] = value
+          end
+        end
+      end
+    end
+    i+=1
+  end
+  return output
+end
+
